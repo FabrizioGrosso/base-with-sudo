ff2169a471090e181bb7d256204ce8f30506a344
FabrizioGrosso <56920116+FabrizioGrosso@users.noreply.github.com>
2025-09-15 14:27:45 +0200

    Update run-ollama.sh

--- run-ollama.sh
+++ run-ollama.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ollama serve &
-ollama list
-ollama pull llama4
+#ollama serve &
+#ollama list
+#ollama pull llama4
 
 ollama serve &
 ollama list
 ollama pull llama3-chatqa
 
-ollama serve &
-ollama list
-ollama pull gpt-oss
+#ollama serve &
+#ollama list
+#ollama pull gpt-oss
